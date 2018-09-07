8927bcf55592b034229f2019663f3a9d732232fb
laudaccurate <laudaccurate@gmail.com>
2018-09-07 11:54:38 +0000

    added dashboard functionalities

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -3,10 +3,19 @@ const { requireLogin } = require("../middleware");
 const User = mongoose.model("User");
 
 module.exports = app => {
+  app.param("userId", async (req, res, next, id) => {
+    try {
+      const user = await User.findById(id);
+      req.user = user;
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  });
+
   app.get("/users/dashboard", requireLogin, async (req, res, next) => {
     try {
       const user = await User.findById(req.session.userId).lean();
-      console.log(Object.keys(user));
       return res.render("dashboard", { user, title: "Dashboard" });
     } catch (error) {
       return next(error);
@@ -16,19 +25,33 @@ module.exports = app => {
   app.get("/users/profile", requireLogin, async (req, res, next) => {
     try {
       const user = await User.findOne({ _id: req.session.userId });
-      console.log(user);
       return res.render("profile", { user, title: "Profile" });
     } catch (error) {
       return next(error);
     }
   });
 
-  app.post("/users/profile/update", requireLogin);
+  app.post("/users/:userId/updateProfile", requireLogin);
 
   app.get("/users/profile/dashboard", requireLogin, async (req, res, next) => {
     try {
       const user = await user.findOne({ _id: req.session.userId });
       return res.render("dashboard", { user });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  app.get("/users/:userId/viewProfile", requireLogin, (req, res, next) => {
+    const user = req.user;
+    return res.render("viewProfile", { user });
+  });
+
+  app.get("/users/neighbours", requireLogin, async (req, res, next) => {
+    try {
+      const user = await User.findById(req.session.userId);
+      const neighbours = await User.find({ _id: { $ne: req.session.userId } });
+      return res.render("neighbours", { neighbours, user });
     } catch (error) {
       return next(error);
     }
